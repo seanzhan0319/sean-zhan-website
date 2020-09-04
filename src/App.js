caeaf12396e21f62ba6df0a8355247b8eb8ad8ab
seanzhan0319 <seanzhan0319@hotmail.com>
2020-09-04 12:39:25 -0400

    Added auto resize for welcome container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import hello from './iceSkating.jpg';
 import './App.css';
+// import 'fontsource-roboto';
+
+import Button from '@material-ui/core/Button';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#e0f7fa'
+    }
+  },
+});
+
+// function openResume() {
+//   window.open()
+// }
 
 function App() {
   return (
-    <div className="App">
-      {/* <body style={{ backgroundImage: `url(${hello})`, backgroundSize: "auto" }}>
-      </body> */}
-    </div>
+    <ThemeProvider theme={theme}>
+      <div class="home">
+        <div class="bg">
+          <div class="group1">
+            <div class="hello">
+              Hi! I'm Sean
+            </div>
+            <div class="resume">
+              <Button 
+                style={{ fontSize:'1.2vw' }} 
+                color="primary" variant="outlined"
+                onClick={() => console.log("hello")}>
+                  Resume  
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
